89ed91ea5125422c86813462ce91d65f06444d99
Miguel Angel Cadena Sanchez <miguelangel_cadena@outlook.com>
2020-11-07 21:37:57 -0600

    Agregar momento al modelo movimientos

--- models/movimientos.go
+++ models/movimientos.go
@@ -1,6 +1,10 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Movimientos es el modelo para las categorias en las que se clasifica un movimiento
 type Movimientos struct {
@@ -9,4 +13,5 @@ type Movimientos struct {
 	TipoID     string             `bson:"tipoid" json:"tipoid"`
 	RubroID    string             `bson:"rubroid" json:"rubroid"`
 	SubrubroID string             `bson:"subrubroid" json:"subrubroid"`
+	Momento    time.Time          `bson:"momento" json:"momento"`
 }
