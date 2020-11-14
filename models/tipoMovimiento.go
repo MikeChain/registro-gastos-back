849c66b89a18ad2038ae188318e96074344b2cf3
Miguel Angel Cadena Sanchez <miguelangel_cadena@outlook.com>
2020-11-14 16:10:40 -0600

    Correcciones en tipoMovimiento

--- models/tipoMovimiento.go
+++ models/tipoMovimiento.go
@@ -4,6 +4,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // TipoMovimiento es el modelo para los tipos de movimiento realizables
 type TipoMovimiento struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Cuenta string             `bson:"cuenta" json:"cuenta"`
+	ID   primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Tipo string             `bson:"tipo" json:"tipo"`
 }
