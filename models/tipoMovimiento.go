85f3f6587d58891d1f90f3067d454f2f2c98f8aa
Miguel Angel Cadena Sanchez <miguelangel_cadena@outlook.com>
2020-11-15 11:34:51 -0600

    Tipos de movimiento base

--- models/tipoMovimiento.go
+++ models/tipoMovimiento.go
@@ -2,8 +2,37 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// TipoMovimiento es el modelo para los tipos de movimiento realizables
-type TipoMovimiento struct {
+// tipoMovimiento es el modelo para los tipos de movimiento realizables
+type tipoMovimiento struct {
 	ID   primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Tipo string             `bson:"tipo" json:"tipo"`
+}
+
+var idIngresos, supErr = primitive.ObjectIDFromHex("000000000000000000000001")
+var idEgresos, _ = primitive.ObjectIDFromHex("000000000000000000000002")
+var idTransferenciaN, _ = primitive.ObjectIDFromHex("000000000000000000000003")
+var idTransferenciaP, _ = primitive.ObjectIDFromHex("000000000000000000000004")
+
+// Ingresos indica entradas de dinero a una cuenta
+var Ingresos tipoMovimiento = tipoMovimiento{
+	idIngresos,
+	"Ingresos",
+}
+
+// Egresos indica salidas de dinero a una cuenta
+var Egresos tipoMovimiento = tipoMovimiento{
+	idEgresos,
+	"Egresos",
+}
+
+// TransferenciaN indica salidas de dinero a una cuenta que entran en otra
+var TransferenciaN tipoMovimiento = tipoMovimiento{
+	idTransferenciaN,
+	"Transferencia (-)",
+}
+
+// TransferenciaP indica entradas de dinero a una cuenta que salen de otra
+var TransferenciaP tipoMovimiento = tipoMovimiento{
+	idTransferenciaP,
+	"Transferencia (+)",
 }
